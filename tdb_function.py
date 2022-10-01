7180c29c58b2e267568654099a27c56bc68f2cec
Andrey <vedischev76@gmail.com>
2022-10-01 19:19:56 +0500

    update sql

--- tdb_function.py
+++ tdb_function.py
@@ -1,68 +1,72 @@
+
 import view
 import sqlite3
 
-def read_file(path : str):
-    path = 'data.txt'
-    with open(path,encoding = 'utf-8', mode = 'r') as file:
-        print(file.read())
 
-def write_in_file(path : str):
-    path = 'data.txt'
-    with open(path,encoding = 'utf-8', mode = 'a') as file:
-        file.write(f'{view.add_contact()}\n')
+# def read_file(path : str):
+#     path = 'data.txt'
+#     with open(path,encoding = 'utf-8', mode = 'r') as file:
+#         print(file.read())
 
-def find_in_file(path : str):
-    path = 'data.txt'
-    with open(path,encoding = 'utf-8', mode = 'r') as file:
-        f = file.read().split(';')
-        f_name = view.find_contact()
-        for i in f:
-            if f_name in i:
-                print(i)
-        else:
-            print('Контакт не найден')
+# def write_in_file(path : str):
+#     path = 'data.txt'
+#     with open(path,encoding = 'utf-8', mode = 'a') as file:
+#         file.write(f'{view.add_contact()}\n')
 
-def delete_in_file(path : str):
-    path = 'data.txt'
-    with open(path,encoding = 'utf-8', mode = 'r+') as file:
-        f = file.read()
-        f = f.split(';')
-        f_name = view.find_contact()
-        for i in f:
-            if f_name in i:
-                    f.remove(i)
-                    break
-        else:
-            print('Контакт не найден')
-    with open(path,encoding = 'utf-8', mode = 'w') as file:
-        file.write(';'.join(f))
+# def find_in_file(path : str):
+#     path = 'data.txt'
+#     with open(path,encoding = 'utf-8', mode = 'r') as file:
+#         f = file.read().split(';')
+#         f_name = view.find_contact()
+#         for i in f:
+#             if f_name in i:
+#                 print(i)
+#         else:
+#             print('Контакт не найден')
+
+# def delete_in_file(path : str):
+#     path = 'data.txt'
+#     with open(path,encoding = 'utf-8', mode = 'r+') as file:
+#         f = file.read()
+#         f = f.split(';')
+#         f_name = view.find_contact()
+#         for i in f:
+#             if f_name in i:
+#                     f.remove(i)
+#                     break
+#         else:
+#             print('Контакт не найден')
+#     with open(path,encoding = 'utf-8', mode = 'w') as file:
+#         file.write(';'.join(f))
                     
 ################################
 # попытка представить работу с файлами базы cqlite
 
-# def read_file():
-#     base = sqlite3.connect('new.db')
-#     cur = base.cursor()
-#     r = cur.execute('SELECT * FROM data').fetchall()
-#     print(r)
+def read_file(base):
+    base = sqlite3.connect('new.db')
+    cur = base.cursor()
+    r = cur.execute('SELECT * FROM data').fetchall()
+    print(r)
 
 
-# def find_in_file():
-#     base = sqlite3.connect('new.db')
-#     cur = base.cursor()
-#     r = cur.execute('SELECT phone FROM data WHERE Name == ?', ('{name}',)).fetchone()
-#     print(r)
+def find_in_file(base):
+    base = sqlite3.connect('new.db')
+    cur = base.cursor()
+    r = cur.execute('SELECT phone FROM data WHERE Name == ?', (view.name,)).fetchone()
+    print(r)
 
 
-# def write_in_file():
-#     base = sqlite3.connect('new.db')
-#     cur = base.cursor()
-#     cur.execute('INSERT INTO data VALUES(?, ?, ?)', ('{name}', '{number}', '{about}}'))
-#     base.commit()
+def write_in_file(base):
+    base = sqlite3.connect('new.db')
+    cur = base.cursor()
+    cur.execute('INSERT INTO data (Name, phone, info) VALUES (?,?,?)', (view.name, view.about, view.number))
+    base.commit()
+    base.close()
 
 
-# def delete_in_file():
-#     base = sqlite3.connect('new.db')
-#     cur = base.cursor()
-#     cur.execute('DELETE FROM data WHERE Name == ?', ('{name}',))
-#     base.commit()
+def delete_in_file(base):
+    base = sqlite3.connect('new.db')
+    cur = base.cursor()
+    cur.execute('DELETE FROM data WHERE Name == ?', (view.name,))
+    base.commit()
+    base.close()
--- view.py
+++ view.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-def add_contact():
-    name = input('Введите имя контакта: ')
-    about = input('Введите описание контакта: ')
-    number = input('Введите номер контакта: ')
-    col = name + ', ' + about + ', ' + number + ';'
-    return col
+# def add_contact():
+#     name = input('Введите имя контакта: ')
+#     about = input('Введите описание контакта: ')
+#     number = input('Введите номер контакта: ')
+#     col = name + ', ' + about + ', ' + number + ';'
+#     return col
 
 def find_contact():
     name = input('Введите имя контакта: ')
@@ -22,9 +22,16 @@ def action():
 
 ###################################
 
-# def path_file():
-#     base = sqlite3.connect('new.db')
-#     base.execute('CREATE TABLE IF NOT EXISTS {} (Name PRIMARY KEY, phone, info)'.format('data'))
-#     base.commit()
-#     path = 'new.db'
-#     return path
+
+name = input('Введите имя контакта: ')
+about = input('Введите описание контакта: ')
+number = input('Введите номер контакта: ')
+    
+    
+
+def path_file():
+    base = sqlite3.connect('new.db')
+    base.execute('CREATE TABLE IF NOT EXISTS {} (Name PRIMARY KEY, phone, info)'.format('data'))
+    base.commit()
+    path = 'new.db'
+    return path
